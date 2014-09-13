9b1e67758cb8ac9c59e40c2525878d6dfe6852b1
Umang Chouhan <uchouhan@optimiscorp.com>
2014-09-13 13:01:11 +0530

    Bump version for ruby 2.1.2.

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,8 +2,8 @@ node.default['passenger']['version'] = '4.0.50'
 
 node.default['passenger']['rbenv'] = {
   enabled: true,
-  version: '1.9.3-p545',
-  binary: "/usr/local/rbenv/versions/1.9.3-p545/bin/ruby",
-  root_path: "/usr/local/rbenv/versions/1.9.3-p545/lib/ruby/gems/1.9.1/gems/passenger-#{node['passenger']['version']}",
-  module_path: "/usr/local/rbenv/versions/1.9.3-p545/lib/ruby/gems/1.9.1/gems/passenger-#{node['passenger']['version']}/buildout/apache2/mod_passenger.so"
+  version: '2.1.2',
+  binary: "/usr/local/rbenv/versions/2.1.2/bin/ruby",
+  root_path: "/usr/local/rbenv/versions/2.1.2/lib/ruby/gems/2.1.0/gems/passenger-#{node['passenger']['version']}",
+  module_path: "/usr/local/rbenv/versions/2.1.2/lib/ruby/gems/2.1.0/gems/passenger-#{node['passenger']['version']}/buildout/apache2/mod_passenger.so"
 }
