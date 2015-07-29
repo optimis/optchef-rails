a68aae2bc12f7b2b902303a82537e1b36701944f
Umang Chouhan <uchouhan@optimisdev.com>
2015-07-28 19:24:40 -0700

    Fix ruby path.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@ node.default['passenger']['version'] = '4.0.50'
 node.default['passenger']['rbenv'] = {
   enabled: true,
   version: '1.9.3',
-  binary: "/usr/local/rbenv/versions/1.9.3p545/bin/ruby",
+  binary: "/usr/local/rbenv/versions/1.9.3-p545/bin/ruby",
   root_path: "/usr/local/rbenv/versions/1.9.3-p545/lib/ruby/gems/1.9.1/gems/passenger-#{node['passenger']['version']}",
-  module_path: "/usr/local/rbenv/versions/1.9.3p545/lib/ruby/gems/1.9.1/gems/passenger-#{node['passenger']['version']}/buildout/apache2/mod_passenger.so"
+  module_path: "/usr/local/rbenv/versions/1.9.3-p545/lib/ruby/gems/1.9.1/gems/passenger-#{node['passenger']['version']}/buildout/apache2/mod_passenger.so"
 }
