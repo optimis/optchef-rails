24f936e366dab1e3f7f0e3739e1f6bc32d34dcee
Umang Chouhan <uchouhan@optimisdev.com>
2015-08-29 18:15:14 -0700

    Fix passenger ruby path.

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,6 +4,6 @@ node.default['passenger']['rbenv'] = {
   enabled: true,
   version: '2.2.2',
   binary: "/usr/local/rbenv/versions/2.2.2/bin/ruby",
-  root_path: "/usr/local/rbenv/versions/2.2.2/lib/ruby/gems/2.1.0/gems/passenger-#{node['passenger']['version']}",
-  module_path: "/usr/local/rbenv/versions/2.2.2/lib/ruby/gems/2.1.0/gems/passenger-#{node['passenger']['version']}/buildout/apache2/mod_passenger.so"
+  root_path: "/usr/local/rbenv/versions/2.2.2/lib/ruby/gems/2.2.0/gems/passenger-#{node['passenger']['version']}",
+  module_path: "/usr/local/rbenv/versions/2.2.2/lib/ruby/gems/2.2.0/gems/passenger-#{node['passenger']['version']}/buildout/apache2/mod_passenger.so"
 }
